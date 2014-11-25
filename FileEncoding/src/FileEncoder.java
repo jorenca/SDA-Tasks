1a6dfbecd99350bb4cc1c56d2e5ba302acedce52
todorjelev36@gmail.com <todorjelev36@gmail.com>
2014-11-25 10:44:40 +0200

    - changes

--- FileEncoding/src/FileEncoder.java
+++ FileEncoding/src/FileEncoder.java
@@ -1,28 +1,28 @@
-import java.util.LinkedList;
 import java.io.IOException;
+import java.util.LinkedList;
 
 /**
- * You have to implement this class. The encryption algorithm is described below.
- * Replace FN with your faculty number.
+ * You have to implement this class. The encryption algorithm is described in the TaskOne.txt file in this repo.
+ * 
  * @author Georgi Gaydarov
+ *
  */
-public interface FileEncoder 
-{
-    /**
-     * Encodes a file with the specified key and saves the result to a given file.
-     * @param sourceFile - path to the initial file
-     * @param destinationFile - path to the result file
-     * @param key - list of replacement bytes
-     * @throws IOException 
-     */
-    public void encode(String sourceFile, String destinationFile, LinkedList<Character> key) throws IOException;
-
-    /**
-     * Decodes a file that was encoded with the above algorithm.
-     * @param encodedFile - path to encoded file
-     * @param destinationFile - path to the result file
-     * @param key - list of replacement bytes that were used to encode the file
-     * @throws IOException 
-     */
-    public void decode(String encodedFile, String destinationFile, LinkedList<Character> key) throws IOException;
+public interface FileEncoder {
+	/**
+	 * Encodes a file with a specified key and saves the result to a given path.
+	 * @param inFile - path to the initial file
+	 * @param outFile - path to the result file
+	 * @param key - list of replacement bytes
+	 * @throws IOException 
+	 */
+	public void encode(String inFile, String outFile, LinkedList<Character> key) throws IOException;
+	
+	/**
+	 * Decodes a file that was encoded with a specified key and saves the result to a given path.
+	 * @param encodedFile - path to encoded file
+	 * @param outFile - path to the result file
+	 * @param key - list of replacement bytes that were used to encode the file
+	 * @throws IOException 
+	 */
+	public void decode(String encodedFile, String outFile, LinkedList<Character> key) throws IOException;
 }
